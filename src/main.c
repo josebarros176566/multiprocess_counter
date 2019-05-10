f41957413659fc9a8a36518517c928901860ea40
josebarros176566 <necomaneco@hotmail.com>
2019-05-10 18:07:00 -0300

    lab atrasado

--- src/main.c
+++ src/main.c
@@ -1,17 +1,144 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
+
+
+       #include <sys/wait.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
 
-int main() {
 
-  int x, y;
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+int main (){
+	
+	
+
+        int* vetor ;
+	int protection = PROT_READ | PROT_WRITE;
+  	int visibility = MAP_SHARED | MAP_ANON;
+	int soma=0;
+	//soma= (int*) mmap(NULL, sizeof(int)*1, protection, visibility, 0, 0);
+	//*soma=0;
+	//char st[80];
+
+	 unsigned int n[80],a,b;
+	pid_t pid[2];
+	int i=0,j=0,c,e,k;
+	
+	
+	do{
+		k= scanf("%u",&a);
+		n[j]=a;
+		j++;
+	}while(k!=EOF);
+
+
+	/*while(st[i]!='\0'){
+		a=atoi(st[i]);
+		n[j]=a;
+		j++;
+		i++;
+		while(st[i]=' ' )
+			i++;
+	}*/
+	j=j-2;
+	i=0;
+/*for(k=0;k<=j;k++){
+	printf("%d\n",n[k]);
+
+}*/
+vetor=(int*)mmap(NULL,j,protection,visibility,-1,0);
+
+while(i<=j){
+	//printf("%d\n",i);
+	if((j-i)>=3){
+			//printf("entrada1\n");
+		for(e=0;e<3;e++){
+			a=n[i];
+			i++;
+			pid[e]=fork();
+			if(pid[e]==0){
+				c=0;
+				for(b=2;b<a;b++){
+					if(a%b==0)
+					c=1;
+				}
+				if(c==0)
+					vetor[i-1]=1;
+				else vetor[i-1]=0;
+				if(a==0||a==1)			
+					vetor[i-1]=0;				
+				exit(0);
+			}	
+		}
+		for(e=0;e<3;e++)
+			waitpid(pid[e], NULL, 0);
+				
+	}
+	else if((j-i)==2){
+		//printf("entrada2\n");		
+		for(e=0;e<2;e++){
+			a=n[i];
+			i++;
+			pid[e]=fork();
+			if(pid[e]==0){
+				c=0;
+				for(b=2;b<a;b++){
+					if(a%b==0)
+					c=1;
+				}
+				if(c==0)
+					vetor[i-1]=1;
+				else vetor[i-1]=0;
+				if(a==0||a==1)			
+					vetor[i-1]=0;				
+				exit(0);
+			}	
+		}
+		for(e=0;e<2;e++)
+			waitpid(pid[e], NULL, 0);
+	}
+	else if((j-i)==1){
+			//printf("entrada3\n");
+		for(e=0;e<1;e++){
+			a=n[i];
+			i++;
+			pid[e]=fork();
+			if(pid[e]==0){
+				c=0;
+				for(b=2;b<a;b++){
+					if(a%b==0)
+					c=1;
+				}
+				if(c==0)
+					vetor[i-1]=1;
+				else vetor[i-1]=0;
+					
+				if(a==0||a==1)			
+					vetor[i-1]=0;				
+				exit(0);
+			}	
+		}
+		for(e=0;e<1;e++)
+			waitpid(pid[e], NULL, 0);
+	}
+	else if ((j-i)==0){
+		break;
+	}
+	
 }
+	
+	
+	for(k=0;k<=i;k++)
+		soma=soma+vetor[k];
+
+	 printf("%d\n",soma);
+	
+	
+	
+}
+	
+ 	
+
